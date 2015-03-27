8855df146369cb4110d391c2933af50fe9878722
hubfactory-hafuka <hafuka31042@gmail.com>
2015-03-27 13:14:58 +0900

    開発中

--- src/main/java/jp/hubfactory/moco/Application.java
+++ src/main/java/jp/hubfactory/moco/Application.java
@@ -5,18 +5,16 @@ import jp.hubfactory.moco.cache.MstGirlMissionCache;
 import jp.hubfactory.moco.cache.MstVoiceCache;
 import jp.hubfactory.moco.cache.MstVoiceSetCache;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-// public class Application extends SpringBootServletInitializer {
-public class Application implements CommandLineRunner {
+ public class Application extends SpringBootServletInitializer implements CommandLineRunner {
 
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+//    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     // @Override
     // protected SpringApplicationBuilder configure(SpringApplicationBuilder
--- src/main/java/jp/hubfactory/moco/WebApplicationConfig.java
+++ src/main/java/jp/hubfactory/moco/WebApplicationConfig.java
@@ -1,20 +1,60 @@
 package jp.hubfactory.moco;
 
-import jp.hubfactory.moco.intercepter.AccessInterceptor;
+import java.sql.DriverManager;
+import java.util.Collections;
 
-import org.springframework.context.annotation.ComponentScan;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.mysql.jdbc.AbandonedConnectionCleanupThread;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan
-public class WebApplicationConfig extends WebMvcConfigurerAdapter {
+//@EnableWebMvc
+//@ComponentScan
+//public class WebApplicationConfig extends WebMvcConfigurerAdapter {
+public class WebApplicationConfig {
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new AccessInterceptor()).addPathPatterns("/**");
+//    /**
+//     * インターセプター
+//     * @param registry
+//     */
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new AccessInterceptor()).addPathPatterns("/**");
+//    }
+
+    @Bean
+    protected ServletContextListener listener() {
+        return new ServletContextListener() {
+
+            @Override
+            public void contextInitialized(ServletContextEvent sce) {
+                // TODO 自動生成されたメソッド・スタブ
+
+            }
+            @Override
+            public void contextDestroyed(ServletContextEvent sce) {
+                System.out.println("########### ServletContextListener#contextDestroyed ############");
+                /*
+                 * Tomcat再起動時に [Abandoned connection cleanup thread] がメモリリークしてるという警告が出る場合の対応
+                 */
+                try {
+                    AbandonedConnectionCleanupThread.shutdown();
+                } catch (final InterruptedException e) {
+                }
+                /*
+                 * SEVERE: A web application registered the JBDC driver [com.mysql.jdbc.Driver] but failed to unregister it when the web application was stopped. To prevent a memory leak, the JDBC Driver has been forcibly unregistered.
+                 * 対応
+                 */
+                Collections.list(DriverManager.getDrivers()).forEach(driver -> {
+                    try {
+                        DriverManager.deregisterDriver(driver);
+                    } catch (final Exception e) {}
+                });
+            }
+        };
     }
 }
